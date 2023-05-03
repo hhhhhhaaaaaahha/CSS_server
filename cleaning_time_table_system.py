0d9137e59de35f130b44fab6183444c1e7ef59d2
Yuko <yuko880129@gmail.com>
2023-05-04 02:28:29 +0800

    123

--- cleaning_time_table_system.py
+++ cleaning_time_table_system.py
@@ -46,5 +46,8 @@ class CleaningTimeTableSystem:
         if len(monthDay)>2:
             return "invalid month and day"
         self.semesterStartD = monthDay
-    def setsemesterEndD(self, monthDay):
-        self.semesterEndD = monthDay
+    def setsemesterEndD(self, token: str, monthDay):
+        if token in self.token_list:
+            self.semesterEndD = monthDay
+        else:
+            return "Permission denied."
